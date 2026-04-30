34d82653d4f839d625fb89d9da415d8b0ca55c06
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-30 10:33:35 -0300

    style: align title center

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -7,12 +7,26 @@ import LayoutDetails from "../layouts/details/LayoutDetails";
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ title: "Disney Characters" }} />
-        <Stack.Screen name="Details" component={LayoutDetails} options={{ title: "Detalhes" }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        title: "Disney Characters",
+                        headerTitleAlign: "center"
+                    }}
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={LayoutDetails}
+                    options={{
+                        title: "Detalhes",
+                        headerTitleAlign: "center"
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
